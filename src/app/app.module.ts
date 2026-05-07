defd5dce4c1c0e3f3f391fe815b461f5ec2eb6c2
Olga Gabueva <118830778+OlgaGabueva@users.noreply.github.com>
2026-05-07 13:56:07 +0200

    version 1

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,37 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
 import { AppComponent } from './app.component';
+import {NgwWowModule} from "ngx-wow";
+import {AppRoutingModule} from "./app-routing.module";
+import { HeaderComponent } from './components/common/header/header.component';
+import { MainComponent } from './components/pages/main/main.component';
+import { CatalogComponent } from './components/pages/catalog/catalog.component';
+import { CatalogSingleProductComponent } from './components/pages/catalog-single-product/catalog-single-product.component';
+import { OrderFormComponent } from './components/pages/order-form/order-form.component';
+import { FooterComponent } from './components/common/footer/footer.component';
+import {HttpClientModule} from "@angular/common/http";
+import {ReactiveFormsModule} from "@angular/forms";
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    FooterComponent,
+    MainComponent,
+    CatalogComponent,
+    CatalogSingleProductComponent,
+    OrderFormComponent,
+
+
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    NgwWowModule,
+    AppRoutingModule,
+    HttpClientModule,
+    ReactiveFormsModule,
+
+
   ],
   providers: [],
   bootstrap: [AppComponent]
